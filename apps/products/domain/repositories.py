2b60a26b9134c7622974dbc4f39af3f1bd64f7c9
JosCarRub <josecarcatarubio6@gmail.com>
2026-01-16 14:32:39 +0100

    feat(products): Define ProductRepository interface

---
+++ apps/products/domain/repositories.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+import uuid
+from apps.products.domain.entities import Product
+
+class ProductRepository(ABC):
+    @abstractmethod
+    def save(self, product: Product) -> None:
+        pass
+
+    @abstractmethod
+    def get_by_id(self, product_id: uuid.UUID) ->Optional[Product]:
+        pass
