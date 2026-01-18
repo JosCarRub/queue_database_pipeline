3491d2620458add3fb6d51243d6cbd09afb92e66
JosCarRub <josecarcatarubio6@gmail.com>
2026-01-18 17:41:35 +0100

    feat(products): Modified endpoint to be asynchronous

--- apps/products/views.py
+++ apps/products/views.py
@@ -2,16 +2,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from decimal import Decimal
-from apps.products.application.use_cases.create_product import CreateProduct
-from apps.products.domain.exceptions import DuplicateProductError, InvalidProductError
+from apps.products.domain.exceptions import PublisherError
 from apps.products.presentation.inyector import Inyector
 from apps.products.presentation.serializers import CreateProductSerializer
 
 
 class ProductListView(APIView):
-    create_product_uc = CreateProduct
-
     def post(self, request):
         serializer = CreateProductSerializer(data=request.data)
         if not serializer.is_valid():
@@ -20,25 +16,18 @@ class ProductListView(APIView):
         validated_data = serializer.validated_data
         
         try:
-            create_uc = Inyector.create_product_uc()
-            new_product = create_uc.execute(
+            request_creation_uc = Inyector.request_product_creation_uc()
+
+            request_creation_uc.execute(
                 name=validated_data['name'],
                 description=validated_data['description'],
                 price=validated_data['price']
             )
 
-            response_data = {
-                'id': str(new_product.id),
-                'name': new_product.name,
-                'description': new_product.description,
-                'price': str(new_product.price),
-                'stock': new_product.stock
-            }
-
-            return Response(response_data, status=status.HTTP_201_CREATED)
+            return Response(
+                {"message": "La petición de creación del producto ha sido aceptada y está siendo procesada."},
+                status=status.HTTP_202_ACCEPTED
+            )
         
-        except DuplicateProductError as e:
-            return Response({'error': str(e)}, status=status.HTTP_409_CONFLICT)
-        
-        except InvalidProductError as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except PublisherError as e:
+            return Response({'error': str(e)}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
