33cd2cffcf1b94f994fc82d52cb7a905581bf1cc
JosCarRub <josecarcatarubio6@gmail.com>
2026-01-18 17:38:44 +0100

    feat(products): Implement service locator to inyect publisher

--- apps/products/presentation/inyector.py
+++ apps/products/presentation/inyector.py
@@ -1,11 +1,14 @@
 
 from apps.products.application.use_cases.check_product_exist_by_name import CheckProductExistsByName
+from apps.products.application.use_cases.request_product_creation import RequestProductCreation
+from apps.products.domain.publishers import ProductPublisher
 from apps.products.domain.repositories import ProductRepository
 
 from apps.products.infrastructure.django_repositories import DjangoProductRepository
 
 from apps.products.application.use_cases.create_product import CreateProduct
 from apps.products.application.use_cases.get_product_by_id import GetProductById
+from apps.products.infrastructure.pika_publisher import PikaProductPublisher
 
 
 class Inyector:
@@ -17,6 +20,10 @@ class Inyector:
     @staticmethod
     def _get_product_repository()-> ProductRepository:
         return DjangoProductRepository()
+    
+    @staticmethod
+    def _get_product_publisher() -> ProductPublisher:
+        return PikaProductPublisher()
     
     @classmethod
     def check_product_exists_by_name_uc(cls) -> CheckProductExistsByName:
@@ -36,3 +43,9 @@ class Inyector:
     def get_product_by_id_uc(cls) -> GetProductById:
         repository = cls._get_product_repository()
         return GetProductById(product_repository=repository)
+    
+    @classmethod
+    def request_product_creation_uc(cls) -> RequestProductCreation:
+        """Fábrica para el caso de uso RequestProductCreation."""
+        publisher = cls._get_product_publisher()
+        return RequestProductCreation(product_publisher=publisher)
