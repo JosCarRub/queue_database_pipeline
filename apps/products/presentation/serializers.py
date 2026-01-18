13443777731ee3681f5f40097c7a80bdc0318d17
JosCarRub <josecarcatarubio6@gmail.com>
2026-01-18 11:04:33 +0100

    fix(serializer): added Decimal validation in price field

--- apps/products/presentation/serializers.py
+++ apps/products/presentation/serializers.py
@@ -4,4 +4,4 @@ from decimal import Decimal
 class CreateProductSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     description = serializers.CharField(required=False,allow_blank=True, default='')
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, min_value='0.00')
+    price = serializers.DecimalField(max_digits=10, decimal_places=2, min_value=Decimal('0.00'))
